62bc11833c5cc93db3c44be9dd2a914af294e7c4
bilaizi <bilaizi@huawei.com>
2022-04-02 05:02:20 +0000

    !232 Add wav demuxer seek support
    Signed-off-by: bilaizi <bilaizi@huawei.com>

--- engine/plugin/plugins/demuxer/wav_demuxer/wav_demuxer_plugin.h
+++ engine/plugin/plugins/demuxer/wav_demuxer/wav_demuxer_plugin.h
@@ -28,6 +28,29 @@ namespace OHOS {
 namespace Media {
 namespace Plugin {
 namespace WavPlugin {
+struct WavHeadAttr {
+    uint8_t  chunkID[4];     // 4 byte RIFF Chunk descriptor
+    uint32_t chunkSize;      // 4 byte RIFF Chunk Size
+    uint8_t  format[4];      // 4 byte The format concern here is "WAVE",
+    // which requires two or three sub-chunks:"fmt", "fact"(optional),and "data"
+
+    uint8_t  subChunk1ID[4]; // 4 byte The "fmt" sub-chunk describes the sound information in the data sub-chunk
+    uint32_t subChunk1Size;  // 4 byte Size of the fmt chunk  16 or 18  18 stand for has auxiliary information
+    uint16_t audioFormat;    // 2 byte Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+    uint16_t numChannels;    // 2 byte Number of channels 1=Mono 2=Sterio
+    uint32_t sampleRate;     // 4 byte Sampling Frequency in Hz
+    uint32_t byteRate;       // 4 byte bytes per second
+    uint16_t blockAlign;     // 2 byte 2=16-bit mono, 4=16-bit stereo
+    uint16_t bitsPerSample;  // 2 byte Number of bits per sample
+
+    uint8_t  subChunk2ID[4]; // 4 byte (optional) The "fact" sub-chunk exists in non-PCM format
+    uint32_t subChunk2Size;  // 4 byte (optional)
+    uint32_t dataFactSize;   // 4 byte (optional)
+
+    uint8_t  subChunk3ID[4]; // 4 byte The "data" sub-chunk indicates the size of the sound information
+    uint32_t subChunk3Size;  // 4 byte Sampled data length
+}; // 根据wav协议构建的结构体，轻易勿动
+
 class WavDemuxerPlugin : public DemuxerPlugin {
 public:
     explicit WavDemuxerPlugin(std::string name);
@@ -57,8 +80,10 @@ private:
     IOContext           ioContext_;
     uint32_t            dataOffset_;
     bool                isSeekable_;
+    uint32_t            wavHeadLength_;
+    WavHeadAttr*        wavHeader_;
 };
-} // namespace Wav
+} // namespace WavPlugin
 } // namespace Plugin
 } // namespace Media
 } // namespace OHOS
