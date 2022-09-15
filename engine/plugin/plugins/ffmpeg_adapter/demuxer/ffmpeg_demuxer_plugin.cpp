1de6702ef66ffc4f2bf49deeb7bb3bb6bed52a62
huyansong <huyansong@huawei.com>
2022-09-15 14:29:46 +0800

    fix video seek wrong position issue
    Signed-off-by: huyansong <huyansong@huawei.com>

--- engine/plugin/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
+++ engine/plugin/plugins/ffmpeg_adapter/demuxer/ffmpeg_demuxer_plugin.cpp
@@ -365,7 +365,7 @@ Status FFmpegDemuxerPlugin::SeekTo(int32_t trackId, int64_t hstTime, SeekMode mo
     auto newTime = ConvertTimeFromFFmpeg(ffTime, avStream->time_base);
     MEDIA_LOG_I("SeekTo " PUBLIC_LOG_U64 " / " PUBLIC_LOG_D64 ", ffTime: " PUBLIC_LOG_D64,
                 newTime, hstTime, ffTime);
-    auto rtv = av_seek_frame(formatContext_.get(), trackId, ffTime, AVSEEK_FLAG_BACKWARD);
+    auto rtv = av_seek_frame(formatContext_.get(), trackId, newTime, AVSEEK_FLAG_BACKWARD);
     if (rtv < 0) {
         MEDIA_LOG_E("seek failed, return value: " PUBLIC_LOG_D32, rtv);
     }
