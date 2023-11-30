6e39795d1dd2acc10dbb3e2df4af73451cad9b73
zhangever <zhangyue57@huawei.com>
2023-11-30 11:28:39 +0000

    update test/unittest/TestAacDemuxerPlugin.cpp.
    Signed-off-by: zhangever <zhangyue57@huawei.com>

--- test/unittest/TestAacDemuxerPlugin.cpp
+++ test/unittest/TestAacDemuxerPlugin.cpp
@@ -30,118 +30,118 @@ std::shared_ptr<AACDemuxerPlugin> AacDemuxerPluginCreate(const std::string& name
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_process, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("process");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
-    auto resetStatus = AacDemuxerPlugin->Reset();
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("process");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
+    auto resetStatus = aacDemuxerPlugin->Reset();
     ASSERT_TRUE(resetStatus == Status::OK);
-    auto initStatus = AacDemuxerPlugin->Init();
+    auto initStatus = aacDemuxerPlugin->Init();
     ASSERT_TRUE(initStatus == Status::OK);
-    auto prepareStatus = AacDemuxerPlugin->Prepare();
+    auto prepareStatus = aacDemuxerPlugin->Prepare();
     ASSERT_TRUE(prepareStatus == Status::OK);
-    auto startStatus = AacDemuxerPlugin->Start();
+    auto startStatus = aacDemuxerPlugin->Start();
     ASSERT_TRUE(startStatus == Status::OK);
-    auto stopStatus = AacDemuxerPlugin->Stop();
+    auto stopStatus = aacDemuxerPlugin->Stop();
     ASSERT_TRUE(stopStatus == Status::OK);
-    auto freeStatus = AacDemuxerPlugin->Deinit();
+    auto freeStatus = aacDemuxerPlugin->Deinit();
     ASSERT_TRUE(freeStatus == Status::OK);
 }
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_get_parameter, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("get parameter");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("get parameter");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
     ValueType para;
-    auto channelLayoutStatus =  AacDemuxerPlugin->GetParameter(Tag::AUDIO_CHANNEL_LAYOUT, para);
+    auto channelLayoutStatus =  aacDemuxerPlugin->GetParameter(Tag::AUDIO_CHANNEL_LAYOUT, para);
     ASSERT_TRUE(channelLayoutStatus == Status::ERROR_UNIMPLEMENTED);
-    auto mediaTypeStatus =  AacDemuxerPlugin->GetParameter(Tag::MEDIA_TYPE, para);
+    auto mediaTypeStatus =  aacDemuxerPlugin->GetParameter(Tag::MEDIA_TYPE, para);
     ASSERT_TRUE(mediaTypeStatus == Status::ERROR_UNIMPLEMENTED);
-    auto sampleRateStatus =  AacDemuxerPlugin->GetParameter(Tag::AUDIO_SAMPLE_RATE, para);
+    auto sampleRateStatus =  aacDemuxerPlugin->GetParameter(Tag::AUDIO_SAMPLE_RATE, para);
     ASSERT_TRUE(sampleRateStatus == Status::ERROR_UNIMPLEMENTED);
-    auto bitrateStatus =  AacDemuxerPlugin->GetParameter(Tag::MEDIA_BITRATE, para);
+    auto bitrateStatus =  aacDemuxerPlugin->GetParameter(Tag::MEDIA_BITRATE, para);
     ASSERT_TRUE(bitrateStatus == Status::ERROR_UNIMPLEMENTED);
-    auto channelsStatus =  AacDemuxerPlugin->GetParameter(Tag::AUDIO_CHANNELS, para);
+    auto channelsStatus =  aacDemuxerPlugin->GetParameter(Tag::AUDIO_CHANNELS, para);
     ASSERT_TRUE(channelsStatus == Status::ERROR_UNIMPLEMENTED);
-    auto trackIdStatus =  AacDemuxerPlugin->GetParameter(Tag::TRACK_ID, para);
+    auto trackIdStatus =  aacDemuxerPlugin->GetParameter(Tag::TRACK_ID, para);
     ASSERT_TRUE(trackIdStatus == Status::ERROR_UNIMPLEMENTED);
-    auto mimeStatus =  AacDemuxerPlugin->GetParameter(Tag::MIME, para);
+    auto mimeStatus =  aacDemuxerPlugin->GetParameter(Tag::MIME, para);
     ASSERT_TRUE(mimeStatus == Status::ERROR_UNIMPLEMENTED);
-    auto streamFormatStatus =  AacDemuxerPlugin->GetParameter(Tag::AUDIO_AAC_STREAM_FORMAT, para);
+    auto streamFormatStatus =  aacDemuxerPlugin->GetParameter(Tag::AUDIO_AAC_STREAM_FORMAT, para);
     ASSERT_TRUE(streamFormatStatus == Status::ERROR_UNIMPLEMENTED);
-    auto mpegVersionStatus =  AacDemuxerPlugin->GetParameter(Tag::AUDIO_MPEG_VERSION, para);
+    auto mpegVersionStatus =  aacDemuxerPlugin->GetParameter(Tag::AUDIO_MPEG_VERSION, para);
     ASSERT_TRUE(mpegVersionStatus == Status::ERROR_UNIMPLEMENTED);
-    auto sampleFormatStatus =  AacDemuxerPlugin->GetParameter(Tag::AUDIO_SAMPLE_FORMAT, para);
+    auto sampleFormatStatus =  aacDemuxerPlugin->GetParameter(Tag::AUDIO_SAMPLE_FORMAT, para);
     ASSERT_TRUE(sampleFormatStatus == Status::ERROR_UNIMPLEMENTED);
-    auto samplePerFrameStatus =  AacDemuxerPlugin->GetParameter(Tag::AUDIO_SAMPLE_PER_FRAME, para);
+    auto samplePerFrameStatus =  aacDemuxerPlugin->GetParameter(Tag::AUDIO_SAMPLE_PER_FRAME, para);
     ASSERT_TRUE(samplePerFrameStatus == Status::ERROR_UNIMPLEMENTED);
-    auto profileStatus =  AacDemuxerPlugin->GetParameter(Tag::AUDIO_AAC_PROFILE, para);
+    auto profileStatus =  aacDemuxerPlugin->GetParameter(Tag::AUDIO_AAC_PROFILE, para);
     ASSERT_TRUE(profileStatus == Status::ERROR_UNIMPLEMENTED);
 }
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_set_parameter, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("get parameter");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
-    ASSERT_TRUE(AacDemuxerPlugin->SetParameter(Tag::AUDIO_CHANNEL_LAYOUT, AudioChannelLayout::STEREO)
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("get parameter");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
+    ASSERT_TRUE(aacDemuxerPlugin->SetParameter(Tag::AUDIO_CHANNEL_LAYOUT, AudioChannelLayout::STEREO)
         == Status::ERROR_UNIMPLEMENTED);
-    ASSERT_TRUE(AacDemuxerPlugin->SetParameter(Tag::MEDIA_TYPE, MediaType::AUDIO) == Status::ERROR_UNIMPLEMENTED);
-    ASSERT_TRUE(AacDemuxerPlugin->SetParameter(Tag::TRACK_ID, 0) == Status::ERROR_UNIMPLEMENTED);
-    ASSERT_TRUE(AacDemuxerPlugin->SetParameter(Tag::MIME, MEDIA_MIME_AUDIO_AAC) == Status::ERROR_UNIMPLEMENTED);
-    ASSERT_TRUE(AacDemuxerPlugin->SetParameter(Tag::AUDIO_SAMPLE_FORMAT, AudioSampleFormat::S16)
+    ASSERT_TRUE(aacDemuxerPlugin->SetParameter(Tag::MEDIA_TYPE, MediaType::AUDIO) == Status::ERROR_UNIMPLEMENTED);
+    ASSERT_TRUE(aacDemuxerPlugin->SetParameter(Tag::TRACK_ID, 0) == Status::ERROR_UNIMPLEMENTED);
+    ASSERT_TRUE(aacDemuxerPlugin->SetParameter(Tag::MIME, MEDIA_MIME_AUDIO_AAC) == Status::ERROR_UNIMPLEMENTED);
+    ASSERT_TRUE(aacDemuxerPlugin->SetParameter(Tag::AUDIO_SAMPLE_FORMAT, AudioSampleFormat::S16)
         == Status::ERROR_UNIMPLEMENTED);
-    ASSERT_TRUE(AacDemuxerPlugin->SetParameter(Tag::AUDIO_SAMPLE_PER_FRAME, 1024) // sample per frame: 1024
+    ASSERT_TRUE(aacDemuxerPlugin->SetParameter(Tag::AUDIO_SAMPLE_PER_FRAME, 1024) // sample per frame: 1024
         == Status::ERROR_UNIMPLEMENTED);
-    ASSERT_TRUE(AacDemuxerPlugin->SetParameter(Tag::AUDIO_AAC_PROFILE, AudioAacProfile::LC)
+    ASSERT_TRUE(aacDemuxerPlugin->SetParameter(Tag::AUDIO_AAC_PROFILE, AudioAacProfile::LC)
         == Status::ERROR_UNIMPLEMENTED);
-    ASSERT_TRUE(AacDemuxerPlugin->SetParameter(Tag::AUDIO_AAC_STREAM_FORMAT, AudioAacStreamFormat::MP4ADTS)
+    ASSERT_TRUE(aacDemuxerPlugin->SetParameter(Tag::AUDIO_AAC_STREAM_FORMAT, AudioAacStreamFormat::MP4ADTS)
         == Status::ERROR_UNIMPLEMENTED);
 }
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_get_allocator, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("get allocator");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
-    auto allocator =  AacDemuxerPlugin->GetAllocator();
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("get allocator");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
+    auto allocator =  aacDemuxerPlugin->GetAllocator();
     ASSERT_TRUE(allocator == nullptr);
 }
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_set_callback, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("set callback");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("set callback");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
     Callback* cb = new Callback();
-    auto status = AacDemuxerPlugin->SetCallback(cb);
+    auto status = aacDemuxerPlugin->SetCallback(cb);
     ASSERT_TRUE(status == Status::OK);
 }
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_get_track_count, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("get track count");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
-    ASSERT_TRUE(AacDemuxerPlugin->GetTrackCount() == 0);
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("get track count");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
+    ASSERT_TRUE(aacDemuxerPlugin->GetTrackCount() == 0);
 }
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_select_track, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("select track");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
-    auto selectStatus = AacDemuxerPlugin->SelectTrack(0);
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("select track");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
+    auto selectStatus = aacDemuxerPlugin->SelectTrack(0);
     ASSERT_TRUE(selectStatus == Status::OK);
 }
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_unselect_track, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("unselect track");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
-    auto unselectStatus = AacDemuxerPlugin->UnselectTrack(0);
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("unselect track");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
+    auto unselectStatus = aacDemuxerPlugin->UnselectTrack(0);
     ASSERT_TRUE(unselectStatus == Status::OK);
 }
 
 HWTEST(TestAacDemuxerPlugin, find_aac_demuxer_plugins_get_select_track, TestSize.Level1)
 {
-    std::shared_ptr<AACDemuxerPlugin> AacDemuxerPlugin = AacDemuxerPluginCreate("get select track");
-    ASSERT_TRUE(AacDemuxerPlugin != nullptr);
+    std::shared_ptr<AACDemuxerPlugin> aacDemuxerPlugin = AacDemuxerPluginCreate("get select track");
+    ASSERT_TRUE(aacDemuxerPlugin != nullptr);
     std::vector<int32_t> trackIds = new std::vector<int32_t>[1];
-    auto selectStatus = AacDemuxerPlugin->GetSelectedTracks(trackIds);
+    auto selectStatus = aacDemuxerPlugin->GetSelectedTracks(trackIds);
     ASSERT_TRUE(selectStatus == Status::OK);
 }
 
